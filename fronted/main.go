b66408acf482de3b9d4c590daa145fea0f2f91aa
oliverwangx <olivwang@umich.edu>
2020-06-03 17:39:36 -0400

    fix sql name order problem

---
+++ fronted/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"context"
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/mvc"
+	"github.com/kataras/iris/sessions"
+	"github.com/opentracing/opentracing-go/log"
+	"imooc-product/common"
+	"imooc-product/fronted/web/controllers"
+	"imooc-product/repositories"
+	"imooc-product/services"
+	"time"
+)
+
+func main() {
+	//1.创建iris 实例
+	app := iris.New()
+	//2.设置错误模式，在mvc模式下提示错误
+	app.Logger().SetLevel("debug")
+	//3.注册模板
+	tmplate := iris.HTML("./fronted/web/views", ".html").Layout("shared/layout.html").Reload(true)
+	app.RegisterView(tmplate)
+	//4.设置模板目标
+	app.HandleDir("/public", "./fronted/web/public")
+	//出现异常跳转到指定页面
+	app.OnAnyErrorCode(func(ctx iris.Context) {
+		ctx.ViewData("message", ctx.Values().GetStringDefault("message", "访问的页面出错！"))
+		ctx.ViewLayout("")
+		ctx.View("shared/error.html")
+	})
+	//连接数据库
+	db, err := common.NewMysqlConn()
+	if err != nil {
+		log.Error(err)
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sess := sessions.New(sessions.Config{
+		Cookie:  "helloworld",
+		Expires: 60 * time.Minute,
+	})
+
+	user := repositories.NewUserRepository("user", db)
+	userService := services.NewService(user)
+	userPro := mvc.New(app.Party("/user"))
+	userPro.Register(userService, ctx, sess.Start)
+	userPro.Handle(new(controllers.UserController))
+
+	app.Run(
+		iris.Addr("0.0.0.0:8082"),
+		iris.WithoutServerError(iris.ErrServerClosed),
+		iris.WithOptimizations,
+	)
+
+}
